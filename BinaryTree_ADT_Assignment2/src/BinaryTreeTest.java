bb0ef58d67ba40f865d2117329ff2d4933202484
Panth112802 <36990771+Panth112802@users.noreply.github.com>
2019-11-11 13:30:32 +0100

    Update test

--- BinaryTree_ADT_Assignment2/src/BinaryTreeTest.java
+++ BinaryTree_ADT_Assignment2/src/BinaryTreeTest.java
@@ -6,7 +6,8 @@ import org.junit.Test;
 
 public class BinaryTreeTest {
 	int root = 8;
-BinaryTree btree = new BinaryTree(root);
+	BinaryTree btree = new BinaryTree(root);
+
 	@Before
 	public void setUp() throws Exception {
 	}
@@ -22,4 +23,30 @@ BinaryTree btree = new BinaryTree(root);
 		assertEquals(3, btree.size());
 	}
 
+	@Test
+	public void testContains() {
+
+		assertTrue(btree.contains(8));
+		btree.addElement(11);
+		assertTrue(btree.contains(11));
+	}
+
+	@Test
+	public void testHeight() {
+		assertEquals(1, btree.height());
+		btree.addElement(10);
+		btree.addElement(11);
+		assertEquals(3, btree.height());
+
+	}
+
+	@Test
+	public void testIsEmpty() {
+		assertFalse(btree.isEmpty());
+	}
+
+	@Test
+	public void testPreOrder() {
+		//how to test this
+	}
 }
