47d2c8c40aa7d409e0abba3c6dfe3fd43c75c8c0
Panth112802 <36990771+Panth112802@users.noreply.github.com>
2019-12-01 13:31:49 +0100

    Update BinaryTreeTest.java

--- BinaryTree_ADT_Assignment2/src/BinaryTreeTest.java
+++ BinaryTree_ADT_Assignment2/src/BinaryTreeTest.java
@@ -8,6 +8,7 @@ public class BinaryTreeTest {
 	int root = 87;
 	BinaryTree btree = new BinaryTree(root);
 	BinaryTree binarytree = new BinaryTree();
+
 	@Before
 	public void setUp() throws Exception {
 	}
@@ -18,7 +19,7 @@ public class BinaryTreeTest {
 
 	@Test
 	public void testSize() {
-		assertEquals(1, btree.size()); //size with only root
+		assertEquals(1, btree.size()); // size with only root
 		btree.addElement(10);
 		btree.addElement(11);
 		assertEquals(3, btree.size());
@@ -40,19 +41,18 @@ public class BinaryTreeTest {
 		btree.addElement(165);
 		btree.addElement(265);
 		btree.addElement(365);
-		assertEquals(0, btree.heightT());
+		assertEquals(2, btree.heightT());
 	}
 
 	@Test
 	public void testIsEmpty() {
-		
+
 		assertTrue(binarytree.isEmpty());
 	}
 
-
 	@Test
 	public void testAddElement() {
-		
+
 		assertEquals(0, binarytree.size());
 		binarytree.addElement(159);
 		assertEquals(1, binarytree.size());
@@ -62,52 +62,62 @@ public class BinaryTreeTest {
 		assertEquals(3, binarytree.size());
 		binarytree.addElement(87);
 		assertEquals(4, binarytree.size());
-	
+
 	}
+
 	@Test
-	public void testInOrder()
-	{
-		BinaryTree tBT = new BinaryTree(7);
-		tBT.addElement(27);
-		tBT.addElement(2);
-		tBT.addElement(29);
-		assertEquals("2 7 27 29", tBT.inOrder());
-		
+	public void testInOrder() {
+		BinaryTree tBT = new BinaryTree(159);
+		tBT.addElement(87);
+		tBT.addElement(20);
+		tBT.addElement(96);
+		assertEquals("20 87 96 159 ", tBT.inOrder());
+
 	}
+
 	@Test
-	public void testPreOrder()
-	{
+	public void testPreOrder() {
 		BinaryTree tBT = new BinaryTree(56);
 		tBT.addElement(6);
 		tBT.addElement(199);
 		tBT.addElement(4);
 
 		assertEquals("56 6 4 199 ", tBT.preOrder());
-		
+
 	}
-	
+
 	@Test
-	public void testPostOrder()
-	{
+	public void testPostOrder() {
 		BinaryTree tBT = new BinaryTree(56);
 		tBT.addElement(6);
 		tBT.addElement(199);
 		tBT.addElement(4);
 
 		assertEquals("4 6 199 56 ", tBT.postOrder());
-		
+
 	}
-	
+
 	@Test
-	public void testLevelOrder()
-	{
+	public void testLevelOrder() {
 		BinaryTree tBT = new BinaryTree(56);
 		tBT.addElement(6);
 		tBT.addElement(199);
 		tBT.addElement(4);
 
 		assertEquals("56 6 199 4 ", tBT.levelOrder());
-		
+
 	}
-	
+
+	@Test
+	public void testGetBalance() {
+		BinaryTreeNode node = new BinaryTreeNode(159);
+		BinaryTreeNode right = new BinaryTreeNode(586);
+
+		node.setRightChild(right);
+		
+		int balance = btree.getBalance(node);
+
+		assertEquals(-1, balance);
+
+	}
 }
